46baf230a5fcdbd1fbc3619b2797ad1848faf157
ht1012 <huyentrang20041012@gmail.com>
2024-12-03 23:27:31 +0700

    assets

---
+++ tlunews/views/news/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Danh sách tin tức</title>
+</head>
+
+<body>
+    <h1>Danh sách tin tức</h1>
+    <ul>
+        <?php foreach ($newsList as $news): ?>
+            <li>
+                <a href="index.php?action=detail&id=<?= $news['id'] ?>">
+                    <?= htmlspecialchars($news['title']) ?> -
+                    (<?= htmlspecialchars($news['category_name']) ?>)
+                </a>
+            </li>
+        <?php endforeach; ?>
+    </ul>
+</body>
+
+</html>
