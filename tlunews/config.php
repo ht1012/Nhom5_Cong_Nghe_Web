640b7195bbcfdf8f1f24fc19240d244619c04788
ht1012 <huyentrang20041012@gmail.com>
2024-12-03 22:31:34 +0700

    MySQL

---
+++ tlunews/config.php
@@ -0,0 +1,12 @@
+<?php
+$host = 'localhost';
+$bdname = 'tlunews';
+$username = 'root';
+$password = '';
+
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$dbname; charset=utf8", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Connection failed: " . $e->getMessage());
+}
