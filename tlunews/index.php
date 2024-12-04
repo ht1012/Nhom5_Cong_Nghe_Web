147a906ca4f65de973f55155a7b61c197bc33953
ht1012 <huyentrang20041012@gmail.com>
2024-12-04 09:19:36 +0700

    database

--- tlunews/index.php
+++ tlunews/index.php
@@ -1,7 +1,7 @@
 <?php
 require_once 'config.php';
 require_once 'controllers/NewsController.php';
-
+require_once 'views/news/index.php';
 $newsController = new NewsController($conn);
 
 $action = $_GET['action'] ?? 'index';
