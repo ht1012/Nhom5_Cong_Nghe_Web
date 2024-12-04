53c8f960f3e86b6f881d25e33e99b16963118a6d
DangNam-IT <dangphuongnam102004@gmail.com>
2024-12-04 19:47:45 +0700

    lấy code các chức năng crud trong nhánh main

--- tlunews/controllers/AdminController.php
+++ tlunews/controllers/AdminController.php
@@ -32,4 +32,59 @@ class AdminController{
             exit;
         }
     }
+
+    public function dashboard() {
+        $this->requireLogin();
+        $news = News::getAllNews();
+        include 'views/admin/dashboard.php';
+    }
+
+    public function addNews() {
+        $this->requireLogin();
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $title = $_POST['title'];
+            $content = $_POST['content'];
+            $image = $_POST['image'];
+            $category_id = $_POST['category_id'];
+            if (News::add($title, $content, $image, $category_id)) {
+                echo "<script>alert('Thêm tin tức thành công');</script>";
+                header('Location: index.php?controller=admin&action=dashboard');
+                exit;
+            }
+        }
+        include 'views/admin/news/add.php';
+    }
+
+    public function editNews() {
+        $this->requireLogin();
+        $id = $_GET['id'];
+        $news = News::getNewsById($id);
+        include 'views/admin/news/edit.php';
+    }
+
+    public function updateNews() {
+        $this->requireLogin();
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $id = $_GET['id'];
+            $title = $_POST['title'];
+            $content = $_POST['content'];
+            $image = $_POST['image'];
+            $category_id = $_POST['category_id'];
+            if (News::update($id, $title, $content, $image, $category_id)) {
+                echo "<script>alert('Sửa tin tức thành công');</script>";
+                header('Location: index.php?controller=admin&action=dashboard');
+                exit;
+            }
+        }
+    }
+
+    public function deleteNews() {
+        $this->requireLogin();
+        $id = $_GET['id'];
+        if (News::delete($id)) {
+            echo "<script>alert('Xóa tin tức thành công');</script>";
+            header('Location: index.php?controller=admin&action=dashboard');
+            exit;
+        }
+    }
 }
