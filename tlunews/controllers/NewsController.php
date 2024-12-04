5eba9327181012550798d27c2440f982651d1d40
Le Doan Tu <ledoantu04@gmail.com>
2024-12-04 10:22:52 +0700

    pull code branch user-details

---
+++ tlunews/controllers/NewsController.php
@@ -0,0 +1,17 @@
+
+<?php
+require_once 'models/News.php';
+
+class NewsController {
+    public function index() {
+        $news = News::getAllNews();
+        include 'views/news/index.php';
+    }
+
+    public function detail($id) {
+        $news = News::getNewsById($id);
+        include 'views/news/detail.php';
+    }
+
+    
+}
