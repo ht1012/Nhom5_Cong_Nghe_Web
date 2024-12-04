904e2ff6ff86af8f72a885da2c6ee36158d1a8e2
DangNam-IT <dangphuongnam102004@gmail.com>
2024-12-04 19:39:36 +0700

    định nghĩa function login

--- tlunews/controllers/AdminController.php
+++ tlunews/controllers/AdminController.php
@@ -3,6 +3,22 @@ require_once 'models/News.php';
 require_once 'models/User.php';
 class AdminController{
     public function login(){
-    
+        if($_SERVER['REQUEST_METHOD'] === 'POST'){
+            $username = $_POST['username'];
+            $password = $_POST['password'];
+            $user = User::authenticate($username, $password);
+            if($user){
+                session_start();
+                $_SESSION['user'] = $user;
+                header('Location: index.php?controller=admin&action=dashboard');
+                exit;  
+            }else{
+                $error = 'Tên đăng nhập hoặc mật khẩu không đúng!';
+            }
+        }
+        include 'views/admin/login.php';
     }
+
+    public function logout(){
+        
 }
