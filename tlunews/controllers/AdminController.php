b93ceed4d5ba79dd88f16575bf0b8fe0501b7342
DangNam-IT <dangphuongnam102004@gmail.com>
2024-12-04 22:06:37 +0700

    update file AdminController

--- tlunews/controllers/AdminController.php
+++ tlunews/controllers/AdminController.php
@@ -28,63 +28,8 @@ class AdminController{
     public function requireLogin(){
         session_start();
         if(!isset($_SESSION['user']) || $_SESSION['user']['role'] != 1){
-            header('Location: index.php?controller=admin&action=login');
+            header('Location: index.php?controller=news&action=index');
             exit;
         }
-    }
-
-    public function dashboard() {
-        $this->requireLogin();
-        $news = News::getAllNews();
-        include 'views/admin/dashboard.php';
-    }
-
-    public function addNews() {
-        $this->requireLogin();
-        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            $title = $_POST['title'];
-            $content = $_POST['content'];
-            $image = $_POST['image'];
-            $category_id = $_POST['category_id'];
-            if (News::add($title, $content, $image, $category_id)) {
-                echo "<script>alert('Thêm tin tức thành công');</script>";
-                header('Location: index.php?controller=admin&action=dashboard');
-                exit;
-            }
-        }
-        include 'views/admin/news/add.php';
-    }
-
-    public function editNews() {
-        $this->requireLogin();
-        $id = $_GET['id'];
-        $news = News::getNewsById($id);
-        include 'views/admin/news/edit.php';
-    }
-
-    public function updateNews() {
-        $this->requireLogin();
-        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            $id = $_GET['id'];
-            $title = $_POST['title'];
-            $content = $_POST['content'];
-            $image = $_POST['image'];
-            $category_id = $_POST['category_id'];
-            if (News::update($id, $title, $content, $image, $category_id)) {
-                echo "<script>alert('Sửa tin tức thành công');</script>";
-                header('Location: index.php?controller=admin&action=dashboard');
-                exit;
-            }
-        }
-    }
-
-    public function deleteNews() {
-        $this->requireLogin();
-        $id = $_GET['id'];
-        if (News::delete($id)) {
-            echo "<script>alert('Xóa tin tức thành công');</script>";
-            header('Location: index.php?controller=admin&action=dashboard');
-            exit;
-        }
-    }
+    }  
 }
