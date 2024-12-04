bfb1aac934fccf43f24294166444731c01df5b3c
ht1012 <huyentrang20041012@gmail.com>
2024-12-04 08:57:23 +0700

    load database

--- tlunews/config.php
+++ tlunews/config.php
@@ -5,7 +5,7 @@ $username = 'root';
 $password = '';
 
 try {
-    $conn = new PDO("mysql:host=$host;dbname=$dbname; charset=utf8", $username, $password);
+    $conn = new PDO("mysql:host=$host;dbname=$bdname; charset=utf8", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Connection failed: " . $e->getMessage());
