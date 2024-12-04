ba2952f22dcca25fc1a01f668ac99b63468bc9d1
DangNam-IT <dangphuongnam102004@gmail.com>
2024-12-04 19:22:46 +0700

    update file

--- tlunews/models/User.php
+++ tlunews/models/User.php
@@ -4,7 +4,7 @@ class User{
     public static function authenticate($username, $password){
         $db = connDB::getConnection();
         $stmt = $db->prepare("SELECT * FROM users WHERE username =? ");
-        $stmt->excute([$username]);
+        $stmt->execute([$username]);
         $user = $stmt->fetch();
 
         if($user && $password === $user["password"]){
