5fd7c41c940909319ad9141bca8f814c6b0dcba8
DangNam-IT <dangphuongnam102004@gmail.com>
2024-12-04 19:08:07 +0700

    update file connDB

---
+++ tlunews/config/connDB.php
@@ -0,0 +1,18 @@
+<?php
+class connDB{
+    private $conn = null;
+    public static function getConnection()
+    {
+        if(self::$conn == null){
+            try{
+                self::$conn = new PDO('mysql:host = localhost; dbname = tlunews','root','',[
+                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+                ]);
+            }catch(PDOException $e){
+                die("Connection failed:". $e->getMessage());
+            }
+        }
+        return self::$conn;
+    }
+}
