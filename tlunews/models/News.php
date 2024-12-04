7d948e2cb26d459650a80756b48e010ea3e60262
ht1012 <huyentrang20041012@gmail.com>
2024-12-04 10:15:55 +0700

    loi

--- tlunews/models/News.php
+++ tlunews/models/News.php
@@ -1,5 +1,5 @@
 <?php
-require_once 'config/connGB.php';
+require_once 'config/connDB.php';
 
 class News
 {
--- tlunews/views/news/index.php
+++ tlunews/views/news/index.php
@@ -12,15 +12,15 @@
     <h1>Danh sách tin tức</h1>
     <div class="row">
         <?php foreach ($news as $item): ?>
-            <div class="col-md-4">
-                <div class="card">
-                    <img src="<?= $item['image'] ?>" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title"><?= $item['title'] ?></h5>
-                        <a href="index.php?action=detail&id=<?= $item['id'] ?>" class="btn btn-primary">Xem chi tiết</a>
-                    </div>
+        <div class="col-md-4">
+            <div class="card">
+                <img src="<?= $item['image'] ?>" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title"><?= $item['title'] ?></h5>
+                    <a href="index.php?action=detail&id=<?= $item['id'] ?>" class="btn btn-primary">Xem chi tiết</a>
                 </div>
             </div>
+        </div>
         <?php endforeach; ?>
     </div>
 </body>
